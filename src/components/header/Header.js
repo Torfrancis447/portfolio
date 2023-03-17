33580111681433f66f1f7a4c1dfe867e53c97e03
tor <torfrancis447@gmail.com>
2023-03-17 19:56:42 -0400

    added resume and profile pic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,26 @@
-import React from "react"
-import "./header.css"
-
+import React from "react";
+import "./header.css";
+import ResumeButton from "./ResumeButton";
+import profileImage from "/home/tor-f/Development/code/my-projects/portfolio/src/assets/profile-pic.jpg"
 const Header = () => {
-    return (
-        <div>
-            Header
+  return (
+    <header>
+      <div class="container">
+        <h5> Hello I'm</h5>
+        <h1>Tor Francis</h1>
+        <h5 class="text-light">Fullstack Developer</h5>
+
+        <ResumeButton />
+
+        <div class="profile-pic">
+            <img src={profileImage} alt="Headshot" />
         </div>
-    )
+
+        <a href="#contact" class="scroll-down"> Scroll Down </a>
+        
+      </div>
+    </header>
+  );
 };
 
-export default Header
+export default Header;
