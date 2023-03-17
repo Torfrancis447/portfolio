b29336bda9c3aae8c5e92d298c4fb631b2aa18d9
tor <torfrancis447@gmail.com>
2023-03-17 14:21:13 -0400

    js config for imports

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import React from "react";
+import Header from "components/header/Header";
+import Navbar from "components/navbar/Navbar";
+import About from "components/about/About";
+import Experience from "components/experience/Experience";
+
 
 
 function App() {
