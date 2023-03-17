8486088eaacf723780cce7a7c12253cecec43628
tor <torfrancis447@gmail.com>
2023-03-17 17:05:51 -0400

    index css setup and media queries

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import Header from "components/header/Header";
 import Navbar from "components/navbar/Navbar";
 import About from "components/about/About";
 import Experience from "components/experience/Experience";
-
+import Footer from "components/footer/Footer";
+import Testimonial from "components/testimonial/Testimonial";
+import Portfolio from "components/portfolio/Porfolio";
+import Contact from "components/contact/Contact";
 
 
 function App() {
   return (
     <div>
-      <h1> Hi </h1>
+      <Header />
+      <Navbar />
+      <Testimonial />
+      <Portfolio />
+      <About />
+      <Experience />
+      <Contact />
+      <Footer />
     </div>
   );
 }
