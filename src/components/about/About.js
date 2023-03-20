61e594dc0f53939fd5ea6e21a651d36c6c49c9fe
tor <torfrancis447@gmail.com>
2023-03-20 18:08:14 -0400

    about section

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,26 +1,53 @@
-import React from "react"
-import "./about.css"
+import React from "react";
+import { FaAward } from "react-icons/fa";
+import {FiUsers } from "react-icons/fi"
+import { VscFolderLibrary } from 'react-icons/vsc'
+import "./about.css";
 
 const About = () => {
-    return (
-        <section id="about">
-            <h5>Get to Know</h5>
-            <h2>About Me</h2>
-            <div>
-                <div class="container about-container">              
-                    <div class="about-me">
-                    <div class="about-me-image">
+  return (
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+      <div class="container about-container">
+        <div class="about-me">
+          <div class="about-me-image">
 
-                    </div>
+          {/* <img src="" alt="" /> */}
+          </div>
+        </div>
 
-                    </div>
-                    <div class="about-content">
+        
+        <div class="about-content">
+          <div class="about-cards">
+          <article class="about-card">
+            <FaAward class="about-icon" />
+            <h5> Experience </h5>
+            <small> Boot Camp Graduate</small>
+          </article>
 
-                    </div>
-                </div>
-            </div>
-        </section>
-    )
+          <article class="about-card">
+            <FiUsers class="about-icon"/>
+            <h5> Clients </h5>
+            <small> </small>
+          </article>
+
+          <article class="about-card">
+            <VscFolderLibrary class="about-icon"/>
+            <h5> Projects </h5>
+            <small> 4+ </small>
+          </article>          
+        </div>
+        <p> Software Engineer with a background in sales and recruiting. 
+        I decided to change fields when I was an IT recruiter and started 
+        talking to a lot on developers about the interesting projects 
+        they have worked on.
+        </p>
+        <a href='#contact' class='btn btn-primary'> Let's Talk</a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default About
+export default About;
