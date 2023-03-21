33752fa1d59cd0bf89dba9bf09e6548fbda36e76
tor <torfrancis447@gmail.com>
2023-03-21 18:43:58 -0400

    css styling and media query

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,77 +1,95 @@
-import React from "react"
-import "./experience.css"
-import {BsPatchCheckFill} from "react-icons/bs"
+import React from "react";
+import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 const Experience = () => {
-    return (
-        <section id="experience">
-        <h5> What Skills I Have</h5> 
-        <h2> My experience </h2>
-        <div class="container experience-container">
-            <div className="experience-frontend">
-                <h3> Frontend Development</h3>
-                <div class="experience-content">
-                    <article class="experience-details">
-                        <BsPatchCheckFill />
-                        <h4> HTML </h4>
-                        <small class="text-light"> Experienced </small>
-                    </article>
-                    <article class="experience-details">
-                        <BsPatchCheckFill />
-                        <h4> CSS </h4>
-                        <small class="text-light"> Experienced </small>
-                    </article>
-                    <article class="experience-details">
-                        <BsPatchCheckFill />
-                        <h4>Bootstrap </h4>
-                        <small class="text-light"> Experienced </small>
-                    </article>
-                    <article class="experience-details">
-                        <BsPatchCheckFill />
-                        <h4> Material UI </h4>
-                        <small class="text-light"> Experienced </small>
-                    </article>
-                    <article class="experience-details">
-                        <BsPatchCheckFill />
-                        <h4> React </h4>
-                        <small class="text-light"> Experienced </small>
-                    </article>                    
-                </div>
-            </div>
-
-            <div class="experience-backend">
+  return (
+    <section id="experience">
+      <h5> What Skills I Have</h5>
+      <h2> My experience </h2>
+      <div class="container experience-container">
+        <div className="experience-frontend">
+          <h3> Frontend Development</h3>
+          <div class="experience-content">
             <article class="experience-details">
-                        <BsPatchCheckFill />
-                        <h4> Ruby </h4>
-                        <small class="text-light"> Experienced </small>
-                    </article>
-                    <article class="experience-details">
-                        <BsPatchCheckFill />
-                        <h4> Ruby on Rails</h4>
-                        <small class="text-light"> Experienced </small>
-                    </article>
-                    <article class="experience-details">
-                        <BsPatchCheckFill />
-                        <h4> PostgreSQL</h4>
-                        <small class="text-light"> Experienced </small>
-                    </article>
-                    <article class="experience-details">
-                        <BsPatchCheckFill />
-                        <div>
-                         <h4> Sqlite3 </h4>
-                         <small class="text-light"> Experienced </small>
-                        </div>
-                    </article>
-                    <article class="experience-details">
-                        <BsPatchCheckFill />
-                        <div>
-                         <h4> APIs </h4>
-                         <small class="text-light"> Experienced </small>
-                        </div>
-                    </article>
-            </div>
+              <BsPatchCheckFill class="experience-details-icon" />
+              <div>
+                <h4> HTML </h4>
+                <small class="text-light"> Experienced </small>
+              </div>
+            </article>
+            <article class="experience-details">
+              <BsPatchCheckFill class="experience-details-icon" />
+              <div>
+                <h4> CSS </h4>
+                <small class="text-light"> Experienced </small>
+              </div>
+            </article>
+            <article class="experience-details">
+              <BsPatchCheckFill class="experience-details-icon" />
+              <div>
+                <h4>Bootstrap </h4>
+                <small class="text-light"> Experienced </small>
+              </div>
+            </article>
+            <article class="experience-details">
+              <BsPatchCheckFill class="experience-details-icon" />
+              <div>
+                <h4> Material UI </h4>
+                <small class="text-light"> Experienced </small>
+              </div>
+            </article>
+            <article class="experience-details">
+              <BsPatchCheckFill class="experience-details-icon" />
+              <div>
+                <h4> React </h4>
+                <small class="text-light"> Experienced </small>
+              </div>
+            </article>
+          </div>
         </div>
-        </section>
-    )
+        <div class="experience-backend">
+          <h3> Backend Development</h3>
+        <div class="experience-content">
+          <article class="experience-details">
+            <BsPatchCheckFill class="experience-details-icon" />
+            <div>
+              <h4> Ruby </h4>
+              <small class="text-light"> Experienced </small>
+            </div>
+          </article>
+          <article class="experience-details">
+            <BsPatchCheckFill class="experience-details-icon" />
+            <div>
+              <h4> Ruby on Rails</h4>
+              <small class="text-light"> Experienced </small>
+            </div>
+          </article>
+          <article class="experience-details">
+            <BsPatchCheckFill class="experience-details-icon" />
+            <div>
+              <h4> PostgreSQL</h4>
+              <small class="text-light"> Experienced </small>
+            </div>
+          </article>
+          <article class="experience-details">
+            <BsPatchCheckFill class="experience-details-icon" />
+            <div>
+              <h4> Sqlite3 </h4>
+              <small class="text-light"> Experienced </small>
+            </div>
+          </article>
+          <article class="experience-details">
+            <BsPatchCheckFill class="experience-details-icon" />
+            <div>
+              <h4> APIs </h4>
+              <small class="text-light"> Experienced </small>
+            </div>
+          </article>
+        </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
-export default Experience
+export default Experience;
