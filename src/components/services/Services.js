2ade79abb45311b75b1561c752037bcf9b1811f4
tor <torfrancis447@gmail.com>
2023-03-21 21:50:29 -0400

    started goal list

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,10 +1,41 @@
 import React from "react"
 import "./services.css"
+import {BiCheck} from "react-icons/bi"
 
 const Services = () => {
     return (
         <section id="services">
-            Services
+            <h5>What I offer</h5>
+            <h2> Services</h2>
+            <div class="container service-container">
+                <article class="service">
+                    <div class="service-head">
+                        <h3>UI/UX Design</h3>
+                    </div>
+                    <ul class="service-list">
+                        <li>
+                            <BiCheck class="service-list-icon"/>
+                            <p> put next project Ideas </p>
+                        </li>
+                        <li>
+                            <BiCheck class="service-list-icon"/>
+                            <p> put next project Ideas </p>
+                        </li>
+                        <li>
+                            <BiCheck class="service-list-icon"/>
+                            <p> put next project Ideas </p>
+                        </li>
+                        <li>
+                            <BiCheck class="service-list-icon"/>
+                            <p> put next project Ideas </p>
+                        </li>
+                        <li>
+                            <BiCheck class="service-list-icon"/>
+                            <p> put next project Ideas </p>
+                        </li>
+                    </ul>
+                </article>
+            </div>
         </section>
     )
 };
